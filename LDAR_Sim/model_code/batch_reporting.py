b574afabdc02d63830970e5956845da50b656fd1
Coleman Vollrath <coleman.vollrath1@ucalgary.ca>
2021-08-12 15:31:37 -0600

    commit
    Former-commit-id: 8734ac78044a2413c8461e1fdc8b0e288f313df7

--- LDAR_Sim/model_code/batch_reporting.py
+++ LDAR_Sim/model_code/batch_reporting.py
@@ -509,7 +509,7 @@ class BatchReporting:
                 dict.update({'Mean Cost': round(df_temp.iloc[:, j].mean())})
                 dict.update({'St. Dev.': df_temp.iloc[:, j].std()})
                 dict.update({'Method': method_lists[i][j].replace('_cost', '')})
-                dict.update({'Total Cost': df_temp.iloc[:, j].sum('Program')})
+                dict.update({'Total Cost': df_temp.iloc[i][j].sum()})
                 rows_list.append(dict)
         df = pd.DataFrame(rows_list)
 
