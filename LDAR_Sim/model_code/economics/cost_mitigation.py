a6af643a749290ba22a01e02f103b13851e9db34
Coleman Vollrath <coleman.vollrath1@ucalgary.ca>
2021-08-19 15:54:10 -0600

    commit
    Former-commit-id: 6c376346300c5aebee0d0d374c02e9c871b2c1d5

--- LDAR_Sim/model_code/economics/cost_mitigation.py
+++ LDAR_Sim/model_code/economics/cost_mitigation.py
@@ -1,14 +1,14 @@
 def cost_mitigation(simulation_dfs, ref_program, base_program):
     total_emissions = [[df['program_name'], df['timeseries']['daily_emissions_kg'].sum()]
                        for df in simulation_dfs]
-    total_daily_cost = [[df['program_name'], df['timeseries']['total_daily_cost']['methods'].sum()]
+    total_daily_cost = [[df['program_name'], df['timeseries']['total_daily_cost'].sum()]
                         for df in simulation_dfs]
 
     # base_dfs = [df for df in simulation_dfs if df['program_name'] == base_program]
     # ref_dfs = [df for df in simulation_dfs if df['program_name'] == ref_program]
 
-    for row in total_emissions[]:
-        total_emissions[i] = ((total_emissions[i] / 1000) * 28)  # covert to tonnes CO2e (GWP 28)
+    for row in total_emissions:
+        total_emissions = ((total_emissions / 1000) * 28)  # covert to tonnes CO2e (GWP 28)
 
     cost_mitigation_ratios = []
     for row in total_daily_cost:
